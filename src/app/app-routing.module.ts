5784d797d5db3ee30280e0392febd3d96eb5e85d
Dev Niklesh <devniklesh@gmail.com>
2021-06-05 23:56:12 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'tire', pathMatch: 'full'},
+  { path: 'tire', loadChildren: () => import('./tire/tire.module').then(m => m.TireModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
